89372041adc86e38d947a4d34e0701289f2a2245
ShiftyLookingCow <whitedcj@outlook.com>
2014-09-13 19:53:15 -0400

    Update http_client.c

--- http_client.c
+++ http_client.c
@@ -1,11 +1,11 @@
-#include <string.h>
-#include <sys/types.h>
+//#include <string.h>
+//#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
+//#include <netdb.h>
 
 int main (int argc, const char* argv[])
 {
