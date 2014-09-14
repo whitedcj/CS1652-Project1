2d911320e04b0dd6e0f67e8033659902621a6538
ShiftyLookingCow <whitedcj@outlook.com>
2014-09-13 20:23:15 -0400

    Update http_client.c

--- http_client.c
+++ http_client.c
@@ -11,7 +11,39 @@ int main (int argc, const char* argv[])
 {
   printf("Started, %s\n %s\n%s\n%s\n%s", argv[0], argv[1], argv[2], argv[3], argv[4]);
   
+  //Parse args
+  char * server_name = argv[2];
+  printf("%s", server_name);
+  int server_port = atoi(argv[3]);
+  printf("%i", server_port);
+  char * server_path = argv[4];
+  printf("%s", server_path);
   
+  printf("Parsed args");
+  
+  //Create and connect to socket
+  int sock=socket(AF_INET,SOCK_STREAM,0);
+  
+  struct sockaddr_in sa;
+  memset(&sa, 0 ,sizeof(sa));
+  sa.sin_port = htons(server_port); //1500
+  sa.sin_addr.s_addr = htonl(gethostbyname(server_name));
+  sa.sin_family = AF_INET;
+  
+  connect(sock, (struct sockaddr *)&sa, sizeof(sa));
+  
+  printf("Connected\n");
+  
+  //Read from server
+  //char * bufout = ”Hello”;
+  //char bufin[80];
+  //write(sock, bufout, strlen(bufout) + 1);
+  //int n = read(sock, bufin, 80);
+  
+  //Close socket
+  close(sock);
+  
+  printf("Closed socket\n");
   
   
   return 0;
