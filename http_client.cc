8fc3938b3824a210c8e0020d7c8eb29ae7343c1b
ShiftyLookingCow <ShiftyLookingCow@users.noreply.github.com>
2014-09-25 20:44:19 -0400

    Update http_client.cc

--- http_client.cc
+++ http_client.cc
@@ -111,7 +111,7 @@ int main(int argc, char * argv[]) {
     		{
     			printf("Possible end of header\n");
     		}
-    	} while(read(sd, c, 1) > 0)
+    	} while(read(sd, c, 1) > 0);
     }
 
     /*close socket and deinitialize */
